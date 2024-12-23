0bfa23f66d58650cfc8f8cb0d36b8b743aec2db9
felipe bartolomeu <felipebart1999@gmail.com>
2024-12-22 22:15:31 -0300

    adicionando try catch

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
   const nameElement = document.querySelector("#name");
   const usernameElement = document.querySelector("#username");
   const avatarElement = document.querySelector("#avatar");
@@ -7,20 +7,29 @@ document.addEventListener("DOMContentLoaded", function () {
   const followingElement = document.querySelector("#following");
   const linkElement = document.querySelector("#link");
 
-  fetch("https://api.github.com/users/felipebart99")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (json) {
-      nameElement.innerText = json.name;
-      usernameElement.innerText = json.login;
-      avatarElement.src = json.avatar_url;
-      followingElement.innerText = json.following;
-      followersElement.innerText = json.followers;
-      reposElement.innerText = json.public_repos;
-      linkElement.href = json.html_url;
-    })
-    .catch(function (error) {
-      console.error("Erro ao buscar dados:", error);
-    });
+  try {
+    const response = await fetch("https://api.github.com/users/felipebart99");
+    if (!response.ok) {
+      throw new Error("Falha ao carregar os dados da API");
+    }
+
+    const json = await response.json();
+
+    nameElement.innerText = json.name;
+    usernameElement.innerText = json.login;
+    avatarElement.src = json.avatar_url;
+    followingElement.innerText = json.following;
+    followersElement.innerText = json.followers;
+    reposElement.innerText = json.public_repos;
+    linkElement.href = json.html_url;
+  } catch (error) {
+    console.error("Erro ao buscar os dados do GitHub:", error);
+
+    nameElement.innerText = "Erro ao carregar os dados";
+    usernameElement.innerText = "";
+    followersElement.innerText = "";
+    followingElement.innerText = "";
+    reposElement.innerText = "";
+    linkElement.href = "#";
+  }
 });
